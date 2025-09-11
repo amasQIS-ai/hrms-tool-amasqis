fafd8a19433f8b8cb69147ce2307363d8c801089
Sudhakar M <sudhakarnan011@gmail.com>
2025-09-11 11:07:56 +0530

    updated Backend url for Production

--- react/src/core/modals/add_company.tsx
+++ react/src/core/modals/add_company.tsx
@@ -20,7 +20,7 @@ import { useUser, useAuth } from "@clerk/clerk-react";
 import { io, Socket } from "socket.io-client";
 
 const AddCompany = () => {
-  const backendUrl = "http://127.0.0.1:5000";
+  const backendUrl = "https://api.devhrms-pm.amasqis.ai/";
 
   const { isSignedIn, user } = useUser();
   const { getToken } = useAuth();
